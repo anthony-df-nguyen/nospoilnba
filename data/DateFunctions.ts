f1b8a65c3617926da83c7a9a4e95127da5d8d4fe
anthony-df-nguyen <skifreetony@yahoo.com>
2023-04-17 21:55:24 -0700

    TS refactor date functions

--- data/DateFunctions.ts
+++ data/DateFunctions.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
-function parseDate(date, mode) {
+function parseDate(date: string, mode: string) {
   let day = date.slice(-2);
   let month = date.slice(5, 7);
   let year = date.slice(0, 4);
@@ -19,29 +19,26 @@ const goToToday = () => {
   return today;
 };
 
-const jump1day = (date,mode) => {
-    let parsed = parseDate(date,"short");
-    let newDay;
-    if (mode === "back") {
-      newDay = moment(new Date(parsed))
-        .subtract(1, "days")
-        .format("yyyy-MM-DD");
-    } else if (mode === "forward") {
-      newDay = moment(new Date(parsed)).add(1, "days").format("yyyy-MM-DD");
-    }
-    sessionStorage.setItem("lastSetDay", newDay);
-    return newDay;
-  };
+const jump1day = (date: string, mode: string) => {
+  let parsed = parseDate(date, "short");
+  let newDay;
+  if (mode === "back") {
+    newDay = moment(new Date(parsed)).subtract(1, "days").format("yyyy-MM-DD");
+  } else if (mode === "forward") {
+    newDay = moment(new Date(parsed)).add(1, "days").format("yyyy-MM-DD");
+  }
+  sessionStorage.setItem("lastSetDay", newDay);
+  return newDay;
+};
 
-const convertPickerDate = (e) => {
-const theDate = moment(new Date(e)).format("L")
-//console.log("🚀 ~ file: App.js ~ line 55 ~ convertPickerDate ~ theDate", theDate)
-const day = theDate.slice(3,5);
-const month = theDate.slice(0,2);
-const year = theDate.slice(-4);
-const useDate = `${year}-${month}-${day}`
-sessionStorage.setItem("lastSetDay",useDate)
-return useDate
-}
+const convertPickerDate = (date: string) => {
+  const theDate = moment(new Date(date)).format("L");
+  const day = theDate.slice(3, 5);
+  const month = theDate.slice(0, 2);
+  const year = theDate.slice(-4);
+  const useDate = `${year}-${month}-${day}`;
+  sessionStorage.setItem("lastSetDay", useDate);
+  return useDate;
+};
 
-export { parseDate, goToToday, jump1day,convertPickerDate };
+export { parseDate, goToToday, jump1day, convertPickerDate };
